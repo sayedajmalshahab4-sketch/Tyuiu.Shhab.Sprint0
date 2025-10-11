9519877da876e9c43bd2a415d219933f3cf512b1
Ajmal <Ajmal@DESKTOP-01V489G>
2025-10-11 05:48:51 -0700

    Task1

---
+++ Tyuiu.Shahab.Spint0.Task1.V0/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.Shahab.Spint0.Task1.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello,Word");
+            string neme = "sayed ajmal shahab";
+            int age = 25;
+            Console.WriteLine("Здравствуйте меня зовут" + neme + "мен " + age + " лет!");
+            Console.ReadKey();
+        }
+    }
+}
